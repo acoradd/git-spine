2d027c16de98b4a69b5cddcdec20ce37e95fbb4a
Thomas DA ROCHA <thomas.darocha@axopen.com>
2026-01-22 09:41:40 +0100

    feat(jewel): remove material

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -17,8 +17,6 @@ kotlin {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.materialIconsExtended)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.preview)
